0094560fe7a52df342752d10939d76e6c50ee8e8
welcome <welcome@Srikanth>
2020-12-22 11:35:21 +0530

    commit 1

--- src/main/java/com/javaprograms/basic/AsciiValue.java
+++ src/main/java/com/javaprograms/basic/AsciiValue.java
@@ -5,7 +5,7 @@ public class AsciiValue {
 	public static void main(String[] args) {
 		
 	
-	char ch='c';
+	char ch='d';
 	int ascii = (int)ch;//casting is converting variable from one type to another, here char variable ch is converted to an int variable castAscii.
 	
 	System.out.println(ascii);
